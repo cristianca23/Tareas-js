5a3afc7fac081cca60195d6c4ce2fe3e4dd3c16f
cristianca23 <cristianca231@gmail.com>
2021-06-01 17:57:00 -0500

    primera versión

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import './styles.css';
+
+import {Todo, TodoList} from './classes';
+import { crearTodoHtml } from './js/componentes';
+
+
+
+export const todoList = new TodoList();
+
+todoList.todos.forEach(todo => crearTodoHtml (todo));
+
+// const newTodo = new Todo ( 'Aprender Javascript');
+// console.log(newTodo);
+
+// console.log('todos', todoList.todos);
